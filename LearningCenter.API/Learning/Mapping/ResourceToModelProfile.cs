c0886c93427bc83d8a2727853c4e5668db256088
Oscar <u201913874@upc.edu.pe>
2022-05-30 15:51:31 -0500

    feat(Category): Implemented DTO Object-Mapping for Category.

--- LearningCenter.API/Learning/Mapping/ResourceToModelProfile.cs
+++ LearningCenter.API/Learning/Mapping/ResourceToModelProfile.cs
@@ -2,6 +2,8 @@ using AutoMapper;
 using LearningCenter.API.Learning.Domain.Models;
 using LearningCenter.API.Learning.Resource;
 
+namespace LearningCenter.API.Learning.Mapping;
+
 public class ResourceToModelProfile : Profile
 {
     public ResourceToModelProfile()
