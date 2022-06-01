d8dda3418bc7225a9a0f89170ffb7f7a840f276f
Oscar <u201913874@upc.edu.pe>
2022-06-01 11:04:16 -0500

    feat(tutorials): Added Incoming and Outgoing Transport Objects.

---
+++ LearningCenter.API/Learning/Resource/TutorialResource.cs
@@ -0,0 +1,10 @@
+namespace LearningCenter.API.Learning.Resource;
+
+public class TutorialResource
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public CategoryResource Category { get; set; }
+    
+}
